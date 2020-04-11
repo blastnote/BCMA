80fa2f6f755b2ea1be3d3099f0448363f392dab3
Niki <nickbrinthaupt@gmail.com>
2020-04-11 15:22:22 -0400

    add dev page link

--- pages/utils/D_scripts.php
+++ pages/utils/D_scripts.php
@@ -1,3 +1,5 @@
+<div class="fixed-bottom d-flex justify-content-end"><a class="text-muted" href="../pages/dev.php">Dev 0.8.3</a></div>
+
 <script type="text/javascript" src="../js/jquery.min.js"></script>
 <script type="text/javascript" src="../js/popper.min.js"></script>
 <script type="text/javascript" src="../js/bootstrap.min.js"></script>
