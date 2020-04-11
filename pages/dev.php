a7b069cd282561a63b4c783cd47c1424327f043c
Niki <nickbrinthaupt@gmail.com>
2020-04-11 15:22:36 -0400

    init dev page

---
+++ pages/dev.php
@@ -0,0 +1,25 @@
+<?php require 'utils/docStart.php' ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <?php require 'utils/D_head.php'; ?>
+</head>
+
+<body>
+<?php require 'utils/nav.php'; ?>
+        
+<!--Main layout-->
+<main class="elegant-color min-vh-100 pt-5">
+  <div class="container-fluid white-text mt-5">
+
+    <h1>Development Documentation & Notes</h1>
+
+  </div>
+</main>
+<!--Main layout-->
+
+<!-- End -->
+<?php require 'utils/D_scripts.php'; ?>
+
+</body>
+</html>
